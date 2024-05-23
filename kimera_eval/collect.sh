a1d38a464d75fa5ab7ffdf3ae5a5af8aed86ee13
Sarika Ramroop <sarika@virtanatech.com>
2024-05-23 16:03:01 -0400

    Adding additional cleanup on SoM

--- kimera_eval/collect.sh
+++ kimera_eval/collect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run command on remote machine using ssh
-# ssh root@MachineB 'bash -s' < local_script.sh
 ssh root@192.168.100.243 'bash -s' < run_kimera.sh
-
+# Copy logs to host machine
 scp 'root@192.168.100.243:/home/root/output*.tar.gz' /home/sarika/indoor_drone_experimentation/kimera_eval/output_logs
+# Cleanup logs on remote
+ssh root@192.168.100.243 'rm -rf /home/root/output*.tar.gz'
